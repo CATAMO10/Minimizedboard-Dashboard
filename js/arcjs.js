b4dae33daa339354e1ff4b776c05d08f40794446
Bingjie Gao <gilman.david04@gmail.com>
2016-02-12 23:08:46 +0000

    arc to be like a guege

--- js/arcjs.js
+++ js/arcjs.js
@@ -1,16 +1,26 @@
 var svg = d3.select("#arcjs")
     .append("svg")
-    .attr("width", 200)
-    .attr("height", 200)
+    .attr("width", 400)
+    .attr("height", 400)
     .append("g")
-    .attr("transform", "translate(100,100)");
+    .attr("transform", "translate(200,300)");
 
 var arc = d3.svg.arc()
-    .innerRadius(50)
+    .innerRadius(180)
     .outerRadius(70)
-    .startAngle(0)
-    .endAngle(2 * Math.PI);
+    .startAngle(1.5 * Math.PI)
+    .endAngle(2*Math.PI);
+var arc2 = d3.svg.arc()
+    .innerRadius(180)
+    .outerRadius(70)
+    .startAngle(2 * Math.PI)
+    .endAngle(2.5*Math.PI);
 
 svg.append("path")
     .attr("class", "arc")
-    .attr("d", arc);
+    .attr("d", arc)
+
+svg.append("path")
+    .attr("class", "arc2")
+    .attr("d", arc2)
+    .attr("fill","red");
